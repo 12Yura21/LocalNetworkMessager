8975d8d6639aa664edf4dfbd19f2e7042bf73aef
Kostiantyn <kostapasenko76@gmail.com>
2024-05-21 16:08:32 +0300

    few changes, back and front are connected

--- Program.cs
+++ Program.cs
@@ -10,8 +10,16 @@ namespace LocalNetworkMessager
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            Controller controller = new Controller();
+            Form1 myForm = new Form1();
+            myForm.Connect += controller.ConnectTo;
+            myForm.Disconnect += controller.Disconnect;
+            myForm.CreateServer += controller.CreateServer;
+            myForm.CloseServer += controller.CloseServer;
+            myForm.SendMessage += controller.Send;
+            controller.MessageReceived += myForm.AddTextToLIst;
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(myForm);
         }
     }
 }
