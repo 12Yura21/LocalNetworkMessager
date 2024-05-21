a1a716d668d0868ac262648a04f089b448adf394
Kostiantyn <kostapasenko76@gmail.com>
2024-05-21 15:13:43 +0300

    Bugs are Fixed

--- Controller.cs
+++ Controller.cs
@@ -7,8 +7,10 @@ namespace LocalNetworkMessager
 	{
 		TcpListener server = new TcpListener(IPAddress.Any, 6666);
 		TcpClient client = new TcpClient(AddressFamily.InterNetwork);
+		List<NetworkStream> clientsInWork = new List<NetworkStream>();
 		public event Action<string> MessageReceived = (X) => { };
 		bool isListening = false;
+		bool amIServer = false;
 		Task Receiver = Task.Run(() => { });
 		public void ConnectTo(string way)     //public event Action<string> Connect;
 		{
@@ -32,6 +34,7 @@ namespace LocalNetworkMessager
 			{
 				MessageReceived?.Invoke("Disconnect or close server firstly!");
 			}
+			amIServer = true;
 			isListening = true;
 			server.Start();
 			MessageReceived?.Invoke("Server Started");
@@ -39,6 +42,7 @@ namespace LocalNetworkMessager
 		}
 		public void CloseServer()     //public event Action CloseServer;
 		{
+			amIServer = false;
 			isListening = false;
 			Receiver.Wait();
 			server.Stop();
@@ -46,7 +50,6 @@ namespace LocalNetworkMessager
 		void ReceiveAsServer()
 		{
 			List<TcpClient> clients = new List<TcpClient>();
-			List<NetworkStream> clientsInWork = new List<NetworkStream>();
 			List<Task> tasks = new List<Task>();
 			TcpClient client;
 			NetworkStream stream;
@@ -65,10 +68,14 @@ namespace LocalNetworkMessager
 			{
 				t.Wait();
 			}
+			foreach (var t in clientsInWork)
+			{
+				t.Dispose();
+			}
+			clientsInWork.Clear();
 		}
 		void SendToAll(List<NetworkStream> streams, string message)
 		{
-			message += '\n';
 			Parallel.For(0, streams.Count, (X) =>
 			{
 				SubSend(streams[X], Encoding.UTF8.GetBytes(message));
@@ -100,7 +107,16 @@ namespace LocalNetworkMessager
 		public void Send(string message)     //public event Action<string> SendMessage;
 		{
 			message = Environment.UserDomainName + message + '\n';
-			SubSend(client.GetStream(), Encoding.UTF8.GetBytes(message));
+			if (amIServer)
+			{
+				message = "Server-" + message;
+				SendToAll(clientsInWork, message);
+				MessageReceived?.Invoke(message);
+			}
+			else
+			{
+				SubSend(client.GetStream(), Encoding.UTF8.GetBytes(message));
+			}
 		}
 		void ReceiveAsClient()
 		{
